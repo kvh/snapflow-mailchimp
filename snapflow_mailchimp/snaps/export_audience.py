876633e17ba5f67d2a67ea0a040292bd5e27efcc
Ken Van Haren <kenvanharen@gmail.com>
2021-04-13 12:39:12 -0700

    Restrict snapflow version

--- snapflow_mailchimp/snaps/export_audience.py
+++ snapflow_mailchimp/snaps/export_audience.py
@@ -18,6 +18,7 @@ if TYPE_CHECKING:
 @Snap(
     "export_audience",
     module="mailchimp",
+    display_name="Export Mailchimp audience"
 )
 @Param("api_key", "str")
 @Param("server", "str")
@@ -33,8 +34,9 @@ def export_audience(
     list_id = ctx.get_param("list_id")
     member_records = members.as_records()
     for record in member_records:
+        member = {k:record.get(k) for k in ["email_address", "status", "merge_field"] if record.get(k) is not None}
         try:
-            response = mailchimp.lists.add_list_member(list_id, record)
+            response = mailchimp.lists.add_list_member(list_id,member)
         except ApiClientError as e:
             # TODO: emit bad row? Error? ctx.emit("error", row) or collect and at end ctx.emit("error", error_records)
             logger.error(e)
